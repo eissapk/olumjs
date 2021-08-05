eb9e85e02f4b188265920fa7be33f13c34aecea6
Eissa Saber <eissapk44@gmail.com>
2021-08-05 21:35:23 +0200

    added translation section in docs page and modified Localize method

--- www/src/app.mjs
+++ www/src/app.mjs
@@ -1,46 +1,12 @@
-import { Olum, debug, $ } from "olum";
+import { Olum, Localize } from "olum";
 import Home from "./views/home.js";
 import Docs from "./views/docs.js";
 import FAQ from "./views/faq.js";
 import Lang from "./views/lang.js";
-
-// translations
 import en from "./locales/en.js";
 import ar from "./locales/ar.js";
-localize({ en, ar });
 
-function localize(obj) {
-  detectDir();
-  String.prototype.i18n = obj;
-  String.prototype.trans = function trans() {
-    const str = this;
-    const locales = this.i18n;
-    const lang = localStorage.getItem("lang") || "en";
-    const langObj = locales[lang];
-    const err = `"${str}" property is misspelled or missed at "src/locales/${lang}.js"`;
-    let translatedStr;
-
-    if (typeof locales != "undefined" && typeof langObj != "undefined") {
-      for (let key in langObj) {
-        if (str === key) translatedStr = langObj[key];
-      }
-    } else {
-      debug("locales or langObj are not defined! @ String.prototype.trans", "warn");
-    }
-
-    return translatedStr || err;
-  };
-}
-
-export function detectDir() {
-  const container = $("#app");
-  const rtlLangs = ["ar"];
-  const currentLang = localStorage.getItem("lang") || "en";
-  if (container) {
-    if (rtlLangs.includes(currentLang)) container.classList.add("RTL");
-    else container.classList.remove("RTL");
-  }
-}
+new Localize({ en, ar }, ["ar"]);
 
 const routes = [
   { path: "/", comp: Home },
